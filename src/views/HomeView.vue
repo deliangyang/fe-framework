e895a4b6eed816c2fa047d18d33b3a2d87cb65ff
ydl <yang623601391@gmail.com>
2022-10-14 15:51:37 +0800

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,17 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <el-button>hello world</el-button>
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import { ElButton } from 'element-plus';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { onMounted } from 'vue';
 
-@Options({
-  components: {
-    HelloWorld,
-  },
+onMounted(() => {
+  console.log(1)
 })
-export default class HomeView extends Vue {}
 </script>
